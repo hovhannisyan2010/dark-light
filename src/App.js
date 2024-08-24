3a4b28b6bc84689653a61f920f40129a3a0ac659
hovhannisyan2010 <hovhannisyanjora2010@gmail.com>
2024-08-24 11:19:17 +0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
+import DarkMode from './Components/DarkMode';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <DarkMode />
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
